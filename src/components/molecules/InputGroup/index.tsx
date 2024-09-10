08df94706628f07b7dc52ba1568e44e39b92b488
rivaldys <ahmadrivaldys@gmail.com>
2024-09-10 13:52:56 +0700

    feat: adding multiple class (style) for component under 'classNames' attribute

--- src/components/molecules/InputGroup/index.tsx
+++ src/components/molecules/InputGroup/index.tsx
@@ -3,15 +3,20 @@ import { ReactNode } from 'react'
 interface InputGroupProps {
     children: ReactNode
     className?: string
+    classNames?: {
+        label: string
+    }
     label?: string
     labelFor?: string
 }
 
-const InputGroup = ({ className, children, label, labelFor }: InputGroupProps) =>
+const InputGroup = ({ className, classNames, children, label, labelFor }: InputGroupProps) =>
 {
+    const customStylesInput = classNames ? ` ${classNames.label}` : ''
+
     return (
         <div className={className}>
-            {label && (<label className="block w-fit text-[#555555] text-sm ml-[5px] mb-[5px]" htmlFor={labelFor}>{label}</label>)}
+            {label && (<label className={`block w-fit text-[#555555] text-sm ml-[5px] mb-[5px]${customStylesInput}`} htmlFor={labelFor}>{label}</label>)}
             {children}
         </div>
     )
