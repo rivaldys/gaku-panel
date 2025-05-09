7072e00cf6b6d842876aa8a08271ef4dd7b111d7
rivaldys <ahmadrivaldys@gmail.com>
2025-05-09 14:29:27 +0700

    chore(tsconfigPaths): add configuration for handle Vite absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()]
+    plugins: [
+        react(),
+        tailwindcss(),
+        tsconfigPaths({
+            projects: ['./tsconfig.app.json']
+        })
+    ]
 })
