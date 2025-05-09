a348a01f519584837265fbe6c61a87a1c58da1da
rivaldys <ahmadrivaldys@gmail.com>
2025-05-09 16:36:18 +0700

    chore: add configuration for handle Vite absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,34 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { ConfigEnv, defineConfig, loadEnv } from 'vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [
-        react(),
-        tailwindcss(),
-        tsconfigPaths({
-            projects: ['./tsconfig.app.json']
-        })
-    ]
-})
+export default ({ mode }: ConfigEnv) => {
+    const env = loadEnv(mode, process.cwd())
+
+    return defineConfig({
+        plugins: [
+            react(),
+            tailwindcss()
+        ],
+        resolve: {
+            alias: {
+                "gaku/assets": "/src/assets",
+                "gaku/images": "/src/assets/images",
+                "gaku/components": "/src/components",
+                "gaku/pages": "/src/pages",
+                "gaku/router": "/src/router",
+                "gaku/services/api": "/src/services/api",
+                "gaku/services/slices": "/src/services/slices",
+                "gaku/services/store": "/src/services/store",
+                "gaku/shared/constants": "/src/shared/constants",
+                "gaku/shared/hooks": "/src/shared/hooks",
+                "gaku/shared/types": "/src/shared/types",
+                "gaku/shared/utils": "/src/shared/utils",
+                "gaku/package": "./package.json"
+            }
+        },
+        server: {
+            port: parseInt(env.VITE_APP_PORT)
+        }
+    })
+}
